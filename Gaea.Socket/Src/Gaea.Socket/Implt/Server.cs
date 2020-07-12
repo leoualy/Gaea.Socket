58117a7bd39407c1c7e8d50035044169b7235978
leoualy <nauyliu@icloud.com>
2020-07-12 23:44:42 +0800

    同步accept完善

--- Gaea.Socket/Src/Gaea.Socket/Implt/Server.cs
+++ Gaea.Socket/Src/Gaea.Socket/Implt/Server.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 using GSocket.Base;
 using GSocket.Connection;
 
@@ -12,7 +13,17 @@ namespace GSocket.Implt
     {
         protected override void Accept(Socket s)
         {
-            
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    Socket sAccept = s.Accept();
+                    if (m_actConnectedHandler != null)
+                    {
+                        m_actConnectedHandler(new ConnectedEventArgs(GetConnection(sAccept)));
+                    }
+                }
+            });
         }
 
         protected override IConnection GetConnection(Socket s)
